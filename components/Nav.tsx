6408d88df5a075d003725d94f0e30a5f3032da6b
minna <yakubuaminat94@gmail.com>
2022-06-14 07:56:51 +0100

    feat: dark mode, navbar

---
+++ components/Nav.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+
+const NavBar = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default NavBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandGrey: "#F9F9F9",
+        minBlack: "#030303",
+      },
+    },
   },
   plugins: [],
-}
+};
