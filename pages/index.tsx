f71b0d0a0e34bf518cb1c6103a73b8a766e2da9a
minna <yakubuaminat94@gmail.com>
2022-09-20 19:06:33 +0100

    feat: layout integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import * as React from "react";
 
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import { useTheme } from "next-themes";
-import GridLayout, { Responsive, WidthProvider } from "react-grid-layout";
+import { Responsive, WidthProvider } from "react-grid-layout";
+import Memoji from "../public/images/memoji-1.png";
 
 import NavBar from "../components/Nav";
 
@@ -15,6 +15,8 @@ const Home: NextPage = () => {
   React.useEffect(() => {
     setMounted(true);
   }, []);
+
+  const ResponsiveGridLayout = WidthProvider(Responsive);
 
   const layout = [
     { i: "blue-eyes-dragon", x: 0, y: 0, w: 3, h: 1, static: true },
@@ -27,6 +29,14 @@ const Home: NextPage = () => {
     { i: "h", x: 2, y: 4, w: 2, h: 1, static: true },
     { i: "i", x: 0, y: 5, w: 3, h: 1, static: true },
   ];
+ 
+  const layouts = {
+    lg: layout,
+    md: layout,
+    // sm: layout,
+    // xs: layout,
+    // xxs: layout
+  }
 
   const renderThemeChanger = () => {
     if (!mounted) return null;
@@ -59,7 +69,7 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen cabinet">
       <Head>
         <title>Portfolio Home Page </title>
         <link rel="icon" href="/favicon.ico" />
@@ -67,14 +77,18 @@ const Home: NextPage = () => {
       <div className="min-container">
         <NavBar />
 
-        <main className="flex w-full flex-1 flex-col px-6 text-center  text-black dark:text-white">
+        <main className="flex w-full flex-1 flex-col px-6 min-h-screen  text-black dark:text-white">
           <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
             toggle
           </button>
            <div className="h-96">
-            <GridLayout layout={layout} cols={3} rowHeight={300} width={1000} margin={[20, 20]}>
-              <div key="blue-eyes-dragon" className="bg-white dark:bg-minLightBlack/30 rounded-md flex items-center justify-center">a</div>
-              <div key="dark-magician" className="bg-white dark:bg-minLightBlack/30 rounded-md flex items-center justify-center">b</div>
+            <ResponsiveGridLayout layouts={layouts} cols={{lg: 3, md: 3, sm: 3, xs: 1, xxs: 1}} rowHeight={300} margin={[20, 20]} breakpoints={{ lg: 1000, md: 996, sm: 768, xs: 480, xxs: 0 }}>
+              <div key="blue-eyes-dragon" className="bg-white dark:bg-minLightBlack/30 rounded-md px-8 py-6">
+                <img src={Memoji.src} alt="memoji" className=" h-[107px] my-6"/>
+                <p className="max-w-[477px]">I'm Aminat, a Frontend Engineer focused on building web applications with React, React Native.</p>
+              </div>
+              <div key="dark-magician" className="bg-white dark:bg-minLightBlack/30 rounded-md flex items-center justify-center">b
+              </div>
               <div key="kuriboh" className="bg-white dark:bg-minLightBlack/30 rounded-md flex items-center justify-center">c</div>
               <div key="spell-caster" className="bg-white dark:bg-minLightBlack/30 rounded-md flex items-center justify-center">D</div>
               <div key="summoned-skull" className="bg-white dark:bg-minLightBlack/30 rounded-md flex items-center justify-center">E</div>
@@ -82,7 +96,7 @@ const Home: NextPage = () => {
               <div key="g" className="bg-white dark:bg-minLightBlack/30 rounded-md flex items-center justify-center">G</div>
               <div key="h" className="bg-white dark:bg-minLightBlack/30 rounded-md flex items-center justify-center">H</div>
               <div key="i" className="bg-white dark:bg-minLightBlack/30 rounded-md flex items-center justify-center">I</div>
-            </GridLayout>
+            </ResponsiveGridLayout>
           </div>
         </main>
        
